8e9624d869cff7f1ac6379516086b827433c97c5
Malolan B <ma101an@icloud.com>
2026-07-15 18:12:04 +0530

    update icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -15,19 +15,22 @@ export default defineConfig({
     description: "Automatically skip intros, recaps & control video.",
     homepage_url: "https://github.com/uchihamalolan/bingeflow",
     permissions: ["tabs", "storage"],
+    action: {
+      default_icon: "icons/128.png",
+      default_title: "Bingeflow",
+    },
+    icons: {
+      16: "icons/16.png",
+      32: "icons/32.png",
+      48: "icons/48.png",
+      128: "icons/128.png",
+    },
     web_accessible_resources: [
       {
         resources: ["netflix-bridge.js"],
         matches: ["*://*.netflix.com/*"],
       },
     ],
-    icons: {
-      16: "icons/16.png",
-      32: "icons/32.png",
-      48: "icons/48.png",
-      96: "icons/96.png",
-      128: "icons/128.png",
-    },
     browser_specific_settings: {
       gecko: {
         id: "bingeflow@malolan.net",
@@ -35,12 +38,6 @@ export default defineConfig({
         data_collection_permissions: {
           required: ["none"],
         },
-      },
-    },
-    action: {
-      default_icon: {
-        96: "icons/96.png",
-        128: "icons/128.png",
       },
     },
   },
