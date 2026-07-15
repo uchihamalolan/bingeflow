98c2c99e938649cc63a119afccc688fdfa33a1e7
Malolan B <ma101an@icloud.com>
2026-07-15 17:41:21 +0530

    Release 0.0.2

--- wxt.config.ts
+++ wxt.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   }),
   manifest: {
     name: "BingeFlow",
-    version: "0.0.1",
+    version: "0.0.2",
     description: "Automatically skip intros, recaps & control video.",
     homepage_url: "https://github.com/uchihamalolan/bingeflow",
     permissions: ["tabs", "storage"],
